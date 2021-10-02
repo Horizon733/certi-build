e910d4f24d418680f1eab3b9d45ee45557e2138e
Avinesh Tripathi <avineshtripathi1@gmail.com>
2021-10-02 22:46:27 +0530

    added circleci folder

--- certi_builder/testing.py
+++
@@ -1,9 +0,0 @@
-import builder
-
-def testtt(self):
-    path = '../certi_builder/test_utillities/testexcel.xlsx'
-    name_list = builder.get_names(path)
-    name = name_list[0]
-    print('abcd' == name)
-
-
